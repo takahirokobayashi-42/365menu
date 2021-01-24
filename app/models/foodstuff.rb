5a2d666af931673deba00af17f80414ce2fe418e
takahirokobayashi-42 <t.kobayashi5820@outlook.jp>
2021-01-24 17:39:24 +0000

    add i-18n

--- app/models/foodstuff.rb
+++ app/models/foodstuff.rb
@@ -2,6 +2,7 @@ class Foodstuff < ApplicationRecord
   belongs_to :category
   has_many :post_dishes
   has_many :dish_foodstuffs
-
+  
+  validates :category_id, :name, :calory, :fat, :protein, :carbohydrate, presence: true
 
 end
