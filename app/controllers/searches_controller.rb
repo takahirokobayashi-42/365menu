32dfff887bbabc3c62db73d33b1f222c501b80a1
takahirokobayashi-42 <t.kobayashi5820@outlook.jp>
2021-01-28 12:03:28 +0000

    change kaminari

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,16 +1,18 @@
 class SearchesController < ApplicationController
   def search
+
     @range = params[:range]
     search = params[:search]
     word= params[:word]
-    
+
     if @range == '1'
       @customer = Customer.search(search,word)
     elsif @range == "2"
       @master_dish = MasterDish.search(search,word)
-      
+      @master_dish = @master_dish.page(params[:page])
     else
       @master_menu = MasterMenu.search(search,word)
+      @master_menu = @master_menu.page(params[:page])
     end
   end
 end
