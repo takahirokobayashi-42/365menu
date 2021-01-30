d101376d303f3b2e4ba3926a7cf5359668a2ab14
takahirokobayashi-42 <t.kobayashi5820@outlook.jp>
2021-01-30 09:24:01 +0000

    fix error

--- app/assets/javascripts/suggest.js
+++ app/assets/javascripts/suggest.js
@@ -1,7 +1,7 @@
 $( function() {
   $( '.suggest' ).autocomplete({
       autoFocus: true,  //テキストフィールドの値をフォーカスされたアイテムの値で置き換える
-      source: "/admins/master_dishes/auto_complete.json",  //サジェストのデータを呼び出す
+      source: "/master_dishes/auto_complete.json",  //サジェストのデータを呼び出す
       minLength: 1  //オートコンプリートが動作する文字数を指定
   });
 });
