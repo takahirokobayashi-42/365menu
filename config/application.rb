76f487c064b973039d77c86608d7aab653ce10dd
takahirokobayashi-42 <t.kobayashi5820@outlook.jp>
2021-02-07 04:54:28 +0000

    fix error

--- config/application.rb
+++ config/application.rb
@@ -16,16 +16,16 @@ module App365menu
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.i18n.default_locale = :ja
-  end
   
-config.generators do |g|
-    g.test_framework :rspec,
-    fixtures: true,
-    view_specs: false,
-    helper_specs: false,
-    routing_specs: false,
-    controller_specs: true,
-    request_specs: false
-    g.fixture_replacement :factory_bot, dir: "spec/factories"
+  config.generators do |g|
+      g.test_framework :rspec,
+      fixtures: true,
+      view_specs: false,
+      helper_specs: false,
+      routing_specs: false,
+      controller_specs: true,
+      request_specs: false
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+    end
   end
 end
